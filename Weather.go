a73f23406b6484ce7009c36c03ce5f83e671eae3
Neshadi Ranaweera <117301828+Neshadi@users.noreply.github.com>
2022-12-02 18:36:49 +0530

    Update Weather.go

--- Weather.go
+++ Weather.go
@@ -5,8 +5,6 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/url"
-	"encoding/json"
-	"strings"
 )
 
 // weather struct
@@ -27,8 +25,9 @@ func main() {
 	var city string
 	fmt.Scanln(&city)
 	APIkey := "75c150e42d3efb8adc8d819361985205"
-	URL := "https://api.openweathermap.org/data/2.5/weather?q= " + city + " + &appid=" + APIkey
-	urlParse, err := url.Parse(URL)
+	curl  "https://api.openweathermap.org/data/2.5/weather?q= " + city + " + &appid=" + APIkey
+	//curl "https://localhost:3000?id=abcdefghi"
+	urlParse, err := url.Parse(curl)
 	resp, err := http.Get(urlParse.String())
 
 	if err != nil {
@@ -38,16 +37,25 @@ func main() {
 	body, err := ioutil.ReadAll(resp.Body)
 	s := string(body)
 	fmt.Println((s))
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		temp := r.URL.Query().Get("temp")
+		fmt.Println("temp =>", temp)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		pressure := r.URL.Query().Get("pressure")
+		fmt.Println("Pressure =>", pressure)
+	})
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		humidity := r.URL.Query().Get("humidity")
+		fmt.Println("Humidity=>", humidity)
+	})
 	//http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	//temp := r.URL.Query().Get("temp")*/
-	fmt.Println(s.temp)
+	//fmt.Println(s.temp)
 	//pressure := r.URL.Query().Get("pressure")
-	fmt.Println(s.pressure)
+	//fmt.Println(s.pressure)
 	//humidity := r.URL.Query().Get("humidity")
-	fmt.Println(s.humidity)
-
-	}
-	
-	
+	//fmt.Println(s.humidity)
 
 }
